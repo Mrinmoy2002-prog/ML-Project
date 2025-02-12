40670bd276cd3992043e45f6c86a3436490efda8
mrinmoy <mrinmoy.low2002@gmail.com>
2025-02-12 20:19:53 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass     # Used to create class variables
 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 # This class is basically create for inputs
 @dataclass
 class DataIngestionConfig:
@@ -48,6 +56,12 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-if __name__== '__main__':
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initate_model_trainer(train_arr,test_arr))
