1be2b36f400eb5be36cb35186c59b4f9d2c7a4f7
mrinmoy <mrinmoy.low2002@gmail.com>
2025-02-12 21:36:47 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -53,3 +53,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+  
+def load_object(file_path):    # This is used to load the pickle files so that they can be used
+    try: 
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
